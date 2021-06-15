463f6c185d97c4c1bc36fabdf4297a078fb43968
Stefanie Gevaert <>
2021-06-15 16:10:57 +0200

    updated the migration file with the old one

---
+++ src/db/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace percipiolondon\companymanagement\db;
+
+
+abstract class Table
+{
+    const CM_COMPANIES = "{{%companymanagement_company}}";
+    const CM_USERS = "{{%companymanagement_users}}";
+    const CM_DOCUMENTS = "{{%companymanagement_documents}}";
+    const CM_COMPANYTYPES = "{{%companymanagement_companytypes}}";
+}
--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -10,279 +10,217 @@
 
 namespace percipiolondon\companymanagement\migrations;
 
-use craft\fields\PlainText;
-use craft\models\FieldGroup;
-use percipiolondon\companymanagement\CompanyManagement;
-
 use Craft;
-use craft\config\DbConfig;
 use craft\db\Migration;
-use yii\base\BaseObject;
+use craft\helpers\MigrationHelper;
+use percipiolondon\companymanagement\db\Table;
+use percipiolondon\companymanagement\elements\Company;
+use yii\base\NotSupportedException;
+
 
 /**
- * Company Management Install Migration
+ * Installation Migration
  *
- * If your plugin needs to create any custom database tables when it gets installed,
- * create a migrations/ folder within your plugin folder, and save an Install.php file
- * within it using the following template:
- *
- * If you need to perform any additional actions on install/uninstall, override the
- * safeUp() and safeDown() methods.
- *
- * @author    Percipio
- * @package   CompanyManagement
- * @since     0.1.0
+ * @author Percipio Global Ltd.
+ * @since 1.0.0
  */
-class Install extends Migration
-{
-    // Public Properties
-    // =========================================================================
+class Install extends Migration {
 
     /**
-     * @var string The database driver to use
-     */
-    public $driver;
-
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * This method contains the logic to be executed when applying this migration.
-     * This method differs from [[up()]] in that the DB logic implemented here will
-     * be enclosed within a DB transaction.
-     * Child classes may implement this method instead of [[up()]] if the DB logic
-     * needs to be within a transaction.
-     *
-     * @return boolean return a false value to indicate the migration fails
-     * and should not proceed further. All other return values mean the migration succeeds.
+     * @inheritdoc
      */
     public function safeUp()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
-        if ($this->createTables()) {
-            $this->createIndexes();
-            $this->addForeignKeys();
-            // Refresh the db schema caches
-            Craft::$app->db->schema->refresh();
-        }
+        $this->createTables();
+        $this->createIndexes();
+        $this->addForeignKeys();
 
         return true;
     }
 
     /**
-     * This method contains the logic to be executed when removing this migration.
-     * This method differs from [[down()]] in that the DB logic implemented here will
-     * be enclosed within a DB transaction.
-     * Child classes may implement this method instead of [[down()]] if the DB logic
-     * needs to be within a transaction.
-     *
-     * @return boolean return a false value to indicate the migration fails
-     * and should not proceed further. All other return values mean the migration succeeds.
+     * @inheritdoc
      */
     public function safeDown()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
-        $this->removeTables();
+        $this->dropForeignKeys();
+        $this->dropTables();
+        $this->dropProjectConfig();
 
-        return true;
+        $this->delete(\craft\db\Table::ELEMENTINDEXSETTINGS, ['type' => [ Company::class ]]);
+        $this->delete(\craft\db\Table::FIELDLAYOUTS, ['type' => [ Company::class ]]);
     }
 
-    // Protected Methods
+    // Protected Functions
     // =========================================================================
 
     /**
-     * Creates the tables needed for the Records used by the plugin
-     *
-     * @return bool
+     * Creates the tables for Company Management
      */
-    protected function createTables()
-    {
-        $tablesCount = 0;
 
-        $tableSchemaCompany = Craft::$app->db->schema->getTableSchema('{{%companymanagement_company}}');
-        $tableSchemaUsers = Craft::$app->db->schema->getTableSchema('{{%companymanagement_users}}');
-        $tableSchemaDocuments = Craft::$app->db->schema->getTableSchema('{{%companymanagement_documents}}');
+    public function createTables()
+    {
+        $tableSchemaCompany = Craft::$app->db->schema->getTableSchema(Table::CM_COMPANIES);
+        $tableSchemaUsers = Craft::$app->db->schema->getTableSchema(Table::CM_USERS);
+        $tableSchemaTypes = Craft::$app->db->schema->getTableSchema(Table::CM_COMPANYTYPES);
+        $tableSchemaDocuments = Craft::$app->db->schema->getTableSchema(Table::CM_DOCUMENTS);
 
         if ($tableSchemaCompany === null) {
-            $tablesCount++;
-            $this->createTable(
-                '{{%companymanagement_company}}',
-                [
-                    'id' => $this->primaryKey(),
-                    'dateCreated' => $this->dateTime()->notNull(),
-                    'dateUpdated' => $this->dateTime()->notNull(),
-                    'uid' => $this->uid(),
-                    // Custom columns in the table
-                    'siteId' => $this->integer()->notNull()->defaultValue(1),
-                    'info' => $this->string()->notNull()->defaultValue(''),
-                    'name' => $this->string()->notNull()->defaultValue(''),
-                    'shortName' => $this->string()->notNull()->defaultValue(''),
-                    'address' => $this->string()->notNull()->defaultValue(''),
-                    'town' => $this->string()->notNull()->defaultValue(''),
-                    'postcode' => $this->string()->notNull()->defaultValue(''),
-                    'registerNumber' => $this->string()->notNull()->defaultValue(''),
-                    'payeReference' => $this->string()->notNull()->defaultValue(''),
-                    'accountsOfficeReference' => $this->string()->notNull()->defaultValue(''),
-                    'taxReference' => $this->string()->notNull()->defaultValue(''),
-                    'website' => $this->string()->notNull()->defaultValue(''),
-                    'logo' => $this->integer(),
-                    'contactFirstName' => $this->string()->notNull()->defaultValue(''),
-                    'contactLastName' => $this->string()->notNull()->defaultValue(''),
-                    'contactEmail' => $this->string()->notNull()->defaultValue(''),
-                    'contactRegistrationNumber' => $this->string()->notNull()->defaultValue(''),
-                    'contactPhone' => $this->string(),
-                    'contactBirthday' => $this->dateTime(),
-                    'userId' => $this->integer()->notNull(),
-                ]
-            );
+            $this->createTable(Table::CM_COMPANIES, [
+                'id' => $this->primaryKey(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+                // Custom columns in the table
+                'siteId' => $this->integer()->notNull()->defaultValue(1),
+                'info' => $this->string()->notNull()->defaultValue(''),
+                'name' => $this->string()->notNull()->defaultValue(''),
+                'shortName' => $this->string()->notNull()->defaultValue(''),
+                'address' => $this->string()->notNull()->defaultValue(''),
+                'town' => $this->string()->notNull()->defaultValue(''),
+                'postcode' => $this->string()->notNull()->defaultValue(''),
+                'registerNumber' => $this->string()->notNull()->defaultValue(''),
+                'payeReference' => $this->string()->notNull()->defaultValue(''),
+                'accountsOfficeReference' => $this->string()->notNull()->defaultValue(''),
+                'taxReference' => $this->string()->notNull()->defaultValue(''),
+                'website' => $this->string()->notNull()->defaultValue(''),
+                'logo' => $this->integer(),
+                'contactFirstName' => $this->string()->notNull()->defaultValue(''),
+                'contactLastName' => $this->string()->notNull()->defaultValue(''),
+                'contactEmail' => $this->string()->notNull()->defaultValue(''),
+                'contactRegistrationNumber' => $this->string()->notNull()->defaultValue(''),
+                'contactPhone' => $this->string(),
+                'contactBirthday' => $this->dateTime(),
+                'userId' => $this->integer(),
+            ]);
+        }
+
+        if ($tableSchemaTypes === null) {
+            $this->createTable(Table::CM_COMPANYTYPES, [
+                'id' => $this->primaryKey(),
+                'fieldLayoutId' => $this->integer(),
+                'name' => $this->string()->notNull(),
+                'handle' => $this->string()->notNull(),
+            ]);
         }
 
         if ($tableSchemaUsers === null) {
-            $tablesCount++;
-            $this->createTable(
-                '{{%companymanagement_users}}',
-                [
-                    'id' => $this->primaryKey(),
-                    'userId' => $this->integer()->notNull(),
-                    'companyId' => $this->integer(),
-                    'dateCreated' => $this->dateTime()->notNull(),
-                    'dateUpdated' => $this->dateTime()->notNull(),
-                    'uid' => $this->uid(),
-                    // Custom columns in the table
-                    'employeeStartDate' => $this->string(),
-                    'employeeEndDate' => $this->string(),
-                    'birthday' => $this->string(),
-                    'nationalInsuranceNumber' => $this->string()->notNull()->defaultValue(''),
-                    'grossIncome' => $this->string()->defaultValue(''),
-                ]
-            );
+            $this->createTable(Table::CM_USERS, [
+                'id' => $this->integer()->notNull(),
+                'companyId' => $this->integer()->notNull(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+                // Custom columns in the table
+                'employeeStartDate' => $this->string(),
+                'employeeEndDate' => $this->string(),
+                'birthday' => $this->string(),
+                'nationalInsuranceNumber' => $this->string()->notNull()->defaultValue(''),
+                'grossIncome' => $this->string()->defaultValue(''),
+                'PRIMARY KEY(id)',
+            ]);
         }
 
         if ($tableSchemaDocuments === null) {
-            $tablesCount++;
-            $this->createTable(
-                '{{%companymanagement_documents}}',
-                [
-                    'userId' => $this->integer()->notNull(),
-                    'assetId' => $this->integer()->notNull(),
-                ]
-            );
-        }
-
-//        $this->execute('alter table {{%companymanagement_company}} modify column id int AUTO_INCREMENT');
-
-        return $tablesCount === 3;
-    }
-
-    /**
-     * Creates the indexes needed for the Records used by the plugin
-     *
-     * @return void
-     */
-    protected function createIndexes()
-    {
-    // companymanagement_company table
-        $this->createIndex(null,'{{%companymanagement_company}}',['id'], true);
-//        $this->createIndex(null,'{{%companymanagement_company}}',['registerNumber'], true);
-
-        // Additional commands depending on the db driver
-        switch ($this->driver) {
-            case DbConfig::DRIVER_MYSQL:
-                break;
-            case DbConfig::DRIVER_PGSQL:
-                break;
+            $this->createTable(Table::CM_DOCUMENTS, [
+                'id' => $this->integer()->notNull(),
+                'userId' => $this->integer()->notNull(),
+                'assetId' => $this->integer()->notNull(),
+                'PRIMARY KEY(id)',
+            ]);
         }
     }
 
     /**
-     * Creates the foreign keys needed for the Records used by the plugin
-     *
-     * @return void
+     * Drop the tables
      */
-    protected function addForeignKeys()
+    public function dropTables()
     {
-        // companymanagement_company table
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_company}}', 'siteId'),
-            '{{%companymanagement_company}}',
-            'siteId',
-            '{{%sites}}',
-            'id',
-            'CASCADE',
-            'CASCADE'
-        );
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_company}}', 'id'),
-            '{{%companymanagement_company}}',
-            'id',
-            '{{%elements}}',
-            'id',
-            'CASCADE'
-        );
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_company}}', 'logo'),
-            '{{%companymanagement_company}}',
-            'logo',
-            '{{%assets}}',
-            'id',
-            'CASCADE'
-        );
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_company}}', 'userId'),
-            '{{%companymanagement_company}}',
-            'userId',
-            '{{%users}}',
-            'id',
-            'CASCADE'
-        );
+        $this->dropTableIfExists(Table::CM_COMPANIES);
+        $this->dropTableIfExists(Table::CM_USERS);
+        $this->dropTableIfExists(Table::CM_DOCUMENTS);
 
-        // companymanagement_users table
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_users}}', 'userId'),
-            '{{%companymanagement_users}}',
-            'userId',
-            '{{%users}}',
-            'id',
-            'CASCADE'
-        );
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_users}}', 'companyId'),
-            '{{%companymanagement_users}}',
-            'companyId',
-            '{{%companymanagement_company}}',
-            'id',
-            'CASCADE'
-        );
-
-        // companymanagement_documents table
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_documents}}', 'assetId'),
-            '{{%companymanagement_documents}}',
-            'assetId',
-            '{{%assets}}',
-            'id',
-            'CASCADE'
-        );
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%companymanagement_documents}}', 'userId'),
-            '{{%companymanagement_documents}}',
-            'userId',
-            '{{%users}}',
-            'id',
-            'CASCADE'
-        );
+        return null;
     }
 
     /**
-     * Removes the tables needed for the Records used by the plugin
-     *
-     * @return void
+     * Drop the foreign keys
      */
-    protected function removeTables()
+    public function dropForeignKeys()
     {
-    // companymanagement_company table
-        $this->dropTableIfExists('{{%companymanagement_users}}');
-        $this->dropTableIfExists('{{%companymanagement_documents}}');
-        $this->dropTableIfExists('{{%companymanagement_company}}');
+        $tables = [
+            Table::CM_COMPANIES,
+            Table::CM_USERS,
+            Table::CM_DOCUMENTS
+        ];
+
+        foreach ($tables as $table) {
+            $this->_dropForeignKeyToAndFromTable($table);
+        }
+    }
+
+    /**
+     * Deletes the project config entry.
+     */
+    public function dropProjectConfig()
+    {
+        Craft::$app->projectConfig->remove('companies');
+    }
+
+    /**
+     * Creates the indexes.
+     */
+    public function createIndexes()
+    {
+//        $this->createIndex(null, Table::CM_COMPANIES, 'typeId', false);
+        $this->createIndex(null, Table::CM_COMPANIES, 'id', false);
+        $this->createIndex(null, Table::CM_COMPANYTYPES, 'handle', true);
+        $this->createIndex(null, Table::CM_COMPANYTYPES, 'fieldLayoutId', true);
+        $this->createIndex(null, Table::CM_USERS, 'companyId', true);
+    }
+
+    /**
+     * Adds the foreign keys.
+     */
+    public function addForeignKeys()
+    {
+        $this->addForeignKey(null, Table::CM_COMPANIES, ['siteId'], \craft\db\Table::SITES, ['id'], 'CASCADE', 'CASCADE');
+        $this->addForeignKey(null, Table::CM_COMPANIES, ['id'], \craft\db\Table::ELEMENTS, ['id'], 'CASCADE', 'CASCADE');
+        $this->addForeignKey(null, Table::CM_COMPANIES, ['logo'], \craft\db\Table::ASSETS, ['id'], 'CASCADE', 'CASCADE');
+        $this->addForeignKey(null, Table::CM_COMPANIES, ['userId'], \craft\db\Table::USERS, ['id'], null, 'CASCADE');
+        $this->addForeignKey(null, Table::CM_USERS, ['id'], \craft\db\Table::USERS, ['id'], 'CASCADE', 'CASCADE');
+        $this->addForeignKey(null, Table::CM_USERS, ['companyId'], Table::CM_COMPANIES, ['id'], 'CASCADE', 'CASCADE');
+        $this->addForeignKey(null, Table::CM_DOCUMENTS, ['userId'], \craft\db\Table::USERS, ['id'], 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * Returns if the table exists.
+     *
+     * @param string $tableName
+     * @param \yii\db\Migration|null $migration
+     * @return bool If the table exists.
+     * @throws NotSupportedException
+     */
+    private function _tableExists(string $tableName): bool
+    {
+        $schema = $this->db->getSchema();
+        $schema->refresh();
+
+        $rawTableName = $schema->getRawTableName($tableName);
+        $table = $schema->getTableSchema($rawTableName);
+
+        return (bool)$table;
+    }
+
+    /**
+     * @param string $tableName
+     * @throws NotSupportedException
+     */
+    private function _dropForeignKeyToAndFromTable(string $tableName)
+    {
+        if ($this->_tableExists($tableName)) {
+            MigrationHelper::dropAllForeignKeysToTable($tableName, $this);
+            MigrationHelper::dropAllForeignKeysOnTable($tableName, $this);
+        }
     }
 }
