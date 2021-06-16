91e5f01410d8ce0f5a1a1af672eac0599aba98b8
michael <michael@dadamotion.be>
2021-06-16 14:52:58 +0200

    Company Interface Generator

--- src/gql/interfaces/elements/Company.php
+++ src/gql/interfaces/elements/Company.php
@@ -20,4 +20,57 @@ use GraphQL\Type\Definition\Type;
  */
 class Company extends Element {
 
+    /**
+     * @inheritdoc
+     */
+    public static function getTypeGenerator(): string
+    {
+        return CompanyType::class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getType($fields = null): Type
+    {
+        if ($type = GqlEntityRegistry::getEntity(self::getName())) {
+            return $type;
+        }
+
+        $type = GqlEntityRegistry::createEntity(self::getName(), new InterfaceType([
+            'name' => static::getName(),
+            'fields' => self::class, '::getFieldDefinitions',
+            'description' => 'This is the interface implemented by all companies.',
+            'resolveType' => function(CompanyElement $value) {
+                return $value->getGqlTypeName();
+            }
+        ]));
+
+        CompanyType::generateTypes();
+
+        return $type;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getName(): string
+    {
+        return 'CompanyInterface';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getFieldDefinition(): array
+    {
+        return TypeManager::prepareFieldDefinitions(array_merge(parent::getFieldDefinitions(), [
+                'url' => [
+                    'name' => 'url',
+                    'type' => Type::string(),
+                    'description' => 'The company\'s full URL',
+                ]
+        ]));
+    }
+
 }
