9de2aacdc1a25831a54210892fe7919ef0b61924
michael <michael@dadamotion.be>
2021-06-16 15:51:39 +0200

    GQL resolvers

---
+++ src/gql/queries/Company.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace percipiolondon\companymanagement\gql\queries;
+
+use percipiolondon\companymanagement\gql\arguments\elements\Company as CompanyArguments;
+use percipiolondon\companymanagement\gql\interfaces\elements\Company as CompanyElement;
+use percipiolondon\companymanagement\gql\resolvers\elements\Company as CompanyResolver;
+
+use craft\gql\base\Query;
+use GraphQL\Type\Definition\Type;
+
+/**
+ * Class Company
+ *
+ * @author Percipio Global Ltd. <support@percipio.london>
+ * @since 1.0.0
+ */
