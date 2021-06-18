e151095bbcae09bf8f3105c3552a3d8082101f26
Stefanie Gevaert <>
2021-06-18 16:39:57 +0200

    fixed typo + provided a sidebar info on edit

--- src/db/Table.php
+++ src/db/Table.php
@@ -8,5 +8,5 @@ abstract class Table
     const CM_USERS = "{{%companymanagement_users}}";
     const CM_DOCUMENTS = "{{%companymanagement_documents}}";
     const CM_COMPANYTYPES = "{{%companymanagement_companytypes}}";
-    const CM_COMAPNYTYPES_SITES = "{{%companymanagement_companytypes_sites}}";
+    const CM_COMPANYTYPES_SITES = "{{%companymanagement_companytypes_sites}}";
 }
